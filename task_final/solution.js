780d83d2940c1d84c95e3aa533c673c2d3b0809f
fenicks1 <fenicks1@gmail.com>
2022-09-26 00:23:51 +0300

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,31 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
- //   let data = {goods: [], order: {}};
-//	let client = name + ' ' + phone;
-		console.log(address);
-	let address1 = address.street + ', ' + address.house + ', ' + address.entrance + ', ' + address.floor + ', ' + address.flat;
+
 	let title;
 	let count;
 	let client;
-	console.log(address1);
-    let data = {client, order: {address, sum}, goods: [{title,count}]};
-	
-	
+
+	let data = {client, order: {address, sum}, goods:[] };	
 	
     let countOfGoods = goods.length;
 
-    for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);		
+    for (let i = 0; i < countOfGoods; i ++) {
+		data.goods.push({title:goods[i].title, count: goods[i].count});
     }
-
-    data.order.address = address.street + ', ' + address.house + ', ' + address.entrance + ', ' + address.floor + ', ' + address.flat;
+	
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
 	data.order.sum = sum;
 
     data.client = name + ' ' + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 	console.log(jsonData);
     return jsonData;
 }
